1904c2cb7f114a51333438a81fd863c5c3787bcf
abarne <celius377@gmail.com>
2021-11-19 20:43:25 -0600

    Add note functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import {nanoid} from "nanoid";
+import NotesList from "./components/NotesList";
+
 const App = () => {
+
+  const[notes, setNotes] = useState([{
+    id:nanoid(),
+    text:"This is the first note!",
+    date: "10/10/2021"
+  },{
+    id:nanoid(),
+    text:"This is the second note!",
+    date: "10/11/2021"
+  },{
+    id:nanoid(),
+    text:"This is the third note!",
+    date: "10/12/2021"
+  },{
+    id:nanoid(),
+    text:"This is the fourth note!",
+    date: "10/13/2021"
+  }]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    setNotes([...notes, {id:nanoid(), text:text, date: date.toLocaleDateString()}]);
+  }
+
   return (
-    <p>Hello React</p>
+    <div className="container">
+      <NotesList notes={notes} handleAddNote={addNote}/>
+    </div>
       )
 };
 
